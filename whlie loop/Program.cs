3a402e52f0ef4dc032400d5c3ea03c352ea9ffe7
Emil Stener <stener99@hotmail.se>
2016-10-14 14:00:12 +0200

    Alla övn

--- whlie loop/Program.cs
+++ whlie loop/Program.cs
@@ -10,6 +10,117 @@ namespace whlie_loop
     {
         static void Main(string[] args)
         {
+
+            //övning1
+            /*
+            int n = 0;
+            while (n++ <100)
+            {
+                Console.WriteLine(n);
+            }
+            Console.ReadKey();
+
+    
+
+
+
+            //övning2
+            bool n = true;
+
+            while (n)
+            { 
+                    string pasword = Console.ReadLine();
+
+                    if (pasword == "axelefage")
+                    {
+                        Console.WriteLine("gzzzz");
+                        n = false;
+                    } 
+                else
+	            { 
+                    Console.WriteLine("fel");
+                }
+                
+            }
+            Console.ReadLine();
+
+            
+
+
+            //övning3
+
+            int i = 1;
+            while(i++ <= 100)
+            
+                    {
+                        Console.WriteLine(i);
+                    }
+                    Console.ReadLine();
+            
+
+        }
+            }
+
+        }
+    
+
+
+
+            //övning2.2
+
+            int i = 10;
+            while (i >= 1)
+                i--;
+
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+            
+
+
+            //övning 2.3
+            int i = 1;
+            while (i == 1)
+                Console.WriteLine(i);
+            {
+
+                
+
+
+            //övning 2.4
+            Console.WriteLine("Gissa på ett tall mellan 1 och 100");
+            Random rnd = new Random();
+            bool vilkor = true;
+            int tal = rnd.Next(100);
+            int i = 0;
+            while (vilkor)
+            {
+                int gissning = int.Parse(Console.ReadLine());
+                if (gissning < tal)
+                {
+                    Console.WriteLine("för lågt");
+                }
+                else if (gissning > tal)
+                {
+                    Console.WriteLine("för högt");
+                }
+                else
+                {
+                    Console.WriteLine("det tog " + i + " försök!");
+                    vilkor = false;
+                }
+                i++;
+            }
+            Console.ReadKey();
+            }
+
+
+
+        */
+        //övning2.5
+
+
         }
     }
 }
